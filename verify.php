3b296f7308252726903e63cee0da04dde188bc65
chanitapol09 <chanitapolsa55@gmail.com>
2017-01-26 17:11:32 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,9 +1,9 @@
 <?php
-$access_token = '1OZId+sFxEGTDwVCcggP4aC+02i2aEsD3K0cVwHM8T1jzPz7IHgwuDPSTgePqcn0OyiWvIVCRsWWPIqhpni3EvOhTfFIUGb7m0JYVDOXtl94+VR+AlAsKXzsMQ0emuumJ+4g4V4Mp6qCi+RAYY+E0AdB04t89/1O/w1cDnyilFU='
+$access_token = '1OZId+sFxEGTDwVCcggP4aC+02i2aEsD3K0cVwHM8T1jzPz7IHgwuDPSTgePqcn0OyiWvIVCRsWWPIqhpni3EvOhTfFIUGb7m0JYVDOXtl94+VR+AlAsKXzsMQ0emuumJ+4g4V4Mp6qCi+RAYY+E0AdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('1498244502 ' . $access_token);
+$headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
