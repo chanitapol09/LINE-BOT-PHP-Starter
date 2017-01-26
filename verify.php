05938669e8b5a355e57219e2e266db2a81c3a7f5
chanitapol09 <chanitapolsa55@gmail.com>
2017-01-26 17:05:31 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '1OZId+sFxEGTDwVCcggP4aC+02i2aEsD3K0cVwHM8T1jzPz7IHgwuDPSTgePqcn0OyiWvIVCRsWWPIqhpni3EvOhTfFIUGb7m0JYVDOXtl94+VR+AlAsKXzsMQ0emuumJ+4g4V4Mp6qCi+RAYY+E0AdB04t89/1O/w1cDnyilFU=';
+$access_token = 1OZId+sFxEGTDwVCcggP4aC+02i2aEsD3K0cVwHM8T1jzPz7IHgwuDPSTgePqcn0OyiWvIVCRsWWPIqhpni3EvOhTfFIUGb7m0JYVDOXtl94+VR+AlAsKXzsMQ0emuumJ+4g4V4Mp6qCi+RAYY+E0AdB04t89/1O/w1cDnyilFU=;
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
