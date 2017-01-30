89e97b236117b934aff53f928ead1f3df24f7903
chanitapol09 <chanitapolsa55@gmail.com>
2017-01-30 22:56:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,12 +17,10 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages => [
-     			 [
-       			 'type' => 'image',
-      			  'originalContentUrl' => 'https://raw.githubusercontent.com/kittinan/Sample-Line-Bot/master/images/beer.jpg',
-      			  'previewImageUrl' => 'https://raw.githubusercontent.com/kittinan/Sample-Line-Bot/master/images/beer_preview.jpg',
-     			 ],			
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
